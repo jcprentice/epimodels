c89fb070496b8bf619c467896a58d2c375851e72
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-03-13 14:14:56 +0000

    Minor improvements using patterns

--- R/plotting__R0.R
+++ R/plotting__R0.R
@@ -12,14 +12,9 @@ plot_R0 <- function(dataset = "fb-final",
 
     # dataset <- "fb-final"; scen <- 1; plot_boxplot = TRUE
 
-    x <- fread(str_glue("{dataset}/data/scen-{scen}-1-out/trace_combine.tsv"))
-
-    x[, str_subset(names(x), "beta|period|_[GE]_", negate = TRUE) := NULL]
-    cols <- names(x)
-
-    Vcols <- str_subset(cols, "G|E")
-    Vcols2 <- str_replace(Vcols, "cov_|r_", "V")
-    setnames(x, Vcols, Vcols2)
+    x <- fread(str_glue("datasets/{dataset}/data/scen-{scen}-1-out/trace_combine.tsv"))
+    x[, names(.SD) := NULL, .SDcols = !patterns("beta|period|_[GE]_")]
+    setnames(x, str_replace(names(x), "cov_|r_", "V"))
 
     # Convert correlation to covariance
     x[, `:=`(
@@ -82,12 +77,12 @@ plot_R0 <- function(dataset = "fb-final",
               measure.vars = r0cols,
               variable.factor = FALSE)
 
-    y[, selection := ifelse(str_ends(variable, "_sel"),
-                            "one sigma", "none")]
-    y[, variable := as.factor(str_remove(variable, "_sel"))]
+    y[, `:=`(selection = fifelse(str_ends(variable, "_sel"),
+                                 "one sigma", "none"),
+             variable = as.factor(str_remove(variable, "_sel")))]
 
 
-    print(y[, .(mean_R0 = mean(value)), .(variable, selection)])
+    y[, .(mean_R0 = mean(value)), .(variable, selection)] |> print()
 
     # dys <- split(y, by = c("variable", "selection")) |>
     #     map(\(x) density(x$value))
